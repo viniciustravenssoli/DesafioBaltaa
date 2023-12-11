b064f41c2cafa5646e7aa4bdd8f6ceca64b8657b
vts1978 <vinicioussouza@gmail.com>
2023-12-11 16:49:00 -0300

    fc

---
+++ Desafio/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BaltaDesafioBlazor.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaltaDesafioBlazor.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+
+    }
+
+    public virtual DbSet<Localidade>? Localidades { get; set; }
+}
---
+++ Desafio/Model/Localidade.cs
@@ -0,0 +1,9 @@
+﻿namespace BaltaDesafioBlazor.Model
+{
+    public class Localidade
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
