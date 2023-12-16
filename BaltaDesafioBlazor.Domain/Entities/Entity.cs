c19e81b1763a42c5d78701812cb80cbc5b2cc1f3
Artur Dias <arturdrm@hotmail.com>
2023-12-16 10:46:00 -0300

    Add locality entity

---
+++ BaltaDesafioBlazor.Domain/Entities/Entity.cs
@@ -0,0 +1,15 @@
+﻿namespace BaltaDesafioBlazor.Domain.Entities;
+
+public abstract class Entity
+{
+    protected Entity()
+    {
+    }
+
+    protected Entity(string id)
+    {
+        Id = id;
+    }
+
+    public string Id { get; private set; } = null!;
+}
---
+++ BaltaDesafioBlazor.Domain/Entities/Locality.cs
@@ -0,0 +1,17 @@
+﻿namespace BaltaDesafioBlazor.Domain.Entities;
+
+public class Locality : Entity
+{
+    protected Locality()
+    {
+    }
+
+    public Locality(string id, string city, string state) : base(id)
+    {
+        City = city;
+        State = state;
+    }
+
+    public string City { get; private set; } = null!;
+    public string State { get; private set; } = null!;
+}
