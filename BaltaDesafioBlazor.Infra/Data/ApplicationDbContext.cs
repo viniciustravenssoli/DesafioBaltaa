0d0ba55d1c2529fe1f4bbf8bbb0ee35fb13049d2
Artur Dias <arturdrm@hotmail.com>
2023-12-16 11:12:10 -0300

    Configure db context - Infra project

---
+++ BaltaDesafioBlazor.Infra/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using BaltaDesafioBlazor.Domain.Entities;
+using BaltaDesafioBlazor.Infra.Data.Map;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaltaDesafioBlazor.Infra.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Locality> Localities { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfigurationsFromAssembly(typeof(LocalityMap).Assembly);
+    }
+}
---
+++ BaltaDesafioBlazor.Infra/Data/Map/LocalityMap.cs
@@ -0,0 +1,35 @@
+﻿using BaltaDesafioBlazor.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BaltaDesafioBlazor.Infra.Data.Map;
+
+public class LocalityMap : IEntityTypeConfiguration<Locality>
+{
+    public void Configure(EntityTypeBuilder<Locality> builder)
+    {
+        builder.ToTable("IBGE");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(i => i.Id)
+            .IsRequired()
+            .HasColumnName("Id")
+            .HasColumnType("CHAR")
+            .HasMaxLength(7);
+
+        builder.Property(i => i.State)
+            .IsRequired()
+            .HasColumnName("State")
+            .HasColumnType("CHAR")
+            .HasMaxLength(2);
+        builder.HasIndex(i => i.State, "IX_IBGE_State");
+
+        builder.Property(i => i.City)
+            .IsRequired()
+            .HasColumnName("City")
+            .HasColumnType("NVARCHAR")
+            .HasMaxLength(80);
+        builder.HasIndex(i => i.City, "IX_IBGE_City");
+    }
+}
