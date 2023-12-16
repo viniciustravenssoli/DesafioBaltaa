eedfccd85b8ee03210d607a3d2374aecc9b9dc63
Artur Dias <arturdrm@hotmail.com>
2023-12-16 17:49:51 -0300

    Add model extensions

---
+++ BaltaDesafioBlazor.Infra/Extensions/ModelExtensions/LocalityModelExtensions.cs
@@ -0,0 +1,18 @@
+﻿using BaltaDesafioBlazor.Domain.Contexts.LocalityContext.Create;
+using BaltaDesafioBlazor.Domain.Contexts.LocalityContext.Update;
+using BaltaDesafioBlazor.Shared.Models.Locality;
+
+namespace BaltaDesafioBlazor.Infra.Extensions.ModelExtensions;
+
+public static class LocalityModelExtensions
+{
+    public static CreateLocalityCommand ToCreateCommand(this LocalityModel model)
+    {
+        return new(model.Id, model.City, model.State);
+    }
+
+    public static UpdateLocalityCommand ToUpdateCommand(this LocalityModel model)
+    {
+        return new(model.Id, model.City, model.State);
+    }
+}
