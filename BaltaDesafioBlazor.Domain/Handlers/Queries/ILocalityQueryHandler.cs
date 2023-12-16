d2e257f1505fc121638b51f5850608d5cae3bcc6
Artur Dias <arturdrm@hotmail.com>
2023-12-16 18:14:23 -0300

    Add query result model

---
+++ BaltaDesafioBlazor.Domain/Handlers/Queries/ILocalityQueryHandler.cs
@@ -0,0 +1,10 @@
+﻿using BaltaDesafioBlazor.Shared.Models.Locality;
+using BaltaDesafioBlazor.Shared.Models.Result;
+
+namespace BaltaDesafioBlazor.Domain.Handlers.Queries;
+
+public interface ILocalityQueryHandler
+{
+    Task<QueryResultModel<LocalityModel>> GetLocalityAsync(string id, CancellationToken cancellationToken = default);
+    Task<QueryResultModel<List<LocalityModel>>> GetLocalitiesAsync(int page = 0, int pageSize = 50, string state = "", string city = "", CancellationToken cancellationToken = default);
+}
---
+++ BaltaDesafioBlazor.Shared/Models/Result/QueryResultModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BaltaDesafioBlazor.Shared.Models.Result;
+
+public class QueryResultModel<T>(bool success, T result)
+{
+    public bool Success { get; } = success;
+    public T Result { get; } = result;
+}
