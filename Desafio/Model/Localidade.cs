359c45eff0cb27d2413bc7757abd9abd8231c442
vts1978 <vinicioussouza@gmail.com>
2023-12-17 17:24:34 -0300

    Removing old models(Locality) in blazor project

--- Desafio/Model/Localidade.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BaltaDesafioBlazor.Model
-{
-    public class Localidade
-    {
-        public int Id { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-    }
-}
